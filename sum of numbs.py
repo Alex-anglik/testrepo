2db8902f05ab09c75723f22f41a09cd8462e4575
Alex-anglik <95584937+Alex-anglik@users.noreply.github.com>
2021-12-12 15:07:55 +0100

    Add files via upload

---
+++ sum of numbs.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 29 01:57:58 2021
+
+@author: Alex
+"""
+
+def sum(n):
+    total = 0
+    for x in n:
+        total= total + x
+    return total 
+
+print(sum((5,2,3)))
+        
